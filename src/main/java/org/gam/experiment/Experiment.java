2ee461ebcfa2a6a63209efc568bddf82b761d99c
Supergamer45550 <gamespcandmobile93@gmail.com>
2024-08-01 03:44:04 +0200

    Add files via upload

--- src/main/java/org/gam/experiment/Experiment.java
+++ src/main/java/org/gam/experiment/Experiment.java
@@ -8,10 +8,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.gam.commands.HealCommand;
 import org.gam.commands.ping;
 import org.gam.commands.testcommand;
 import org.bukkit.ChatColor;
-
 
 public final class Experiment extends JavaPlugin {
 
@@ -19,25 +19,34 @@ public final class Experiment extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
 
-        // Load the config value
+
         boolean isDamageListenerEnabled = getConfig().getBoolean("damage-listener-enabled");
 
-        // Register the damage listener if enabled
+
         if (isDamageListenerEnabled) {
             getServer().getPluginManager().registerEvents(new DamageListener(), this);
         }
 
+
         PluginManager pluginManager = Bukkit.getPluginManager();
-        pluginManager.registerEvents((Listener)new JoinListener(), (Plugin)this);
-        pluginManager.registerEvents((Listener)new BlockListener(), (Plugin)this);
+        pluginManager.registerEvents(new JoinListener(), this);
+        pluginManager.registerEvents(new BlockListener(), this);
+
+
         this.getCommand("testcommand").setExecutor(new testcommand());
         this.getCommand("ping").setExecutor(new ping());
-        getLogger().info(ChatColor.RED  + "GAM-TEST" + ChatColor.GRAY + " wurde aktiviert!");
 
+
+        if (getConfig().getBoolean("heal-command-enabled")) {
+            this.getCommand("heal").setExecutor(new HealCommand(this));
+        }
+
+        getLogger().info(ChatColor.RED + "GAM-TEST" + ChatColor.GRAY + " wurde aktiviert!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info(ChatColor.RED  + "GAM-TEST" + ChatColor.GRAY + " wurde deaktiviert!");
+        getLogger().info(ChatColor.RED + "GAM-TEST" + ChatColor.GRAY + " wurde deaktiviert!");
     }
 }
+
